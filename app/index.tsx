272a32ec0bacd1fafaddd90b127b26745b8808b8
akshay <akshayajith40@gmail.com>
2024-12-05 14:21:51 +0530

    feat: product description completed

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import { View } from "react-native";
+import { ScrollView, StyleSheet, View } from "react-native";
 import ProductImages from "@/components/screens/ProductImages";
 import { MainImageContext } from "@/context/MainImageContex";
 import ColorSelect from "@/components/screens/ColorSelect";
+import ProductDescription from "@/components/screens/ProductDescription";
 
   const imagesArr = {
     main: "https://www.ikea.com/in/en/images/products/ekeroe-armchair-skiftebo-yellow__0204753_pe359787_s5.jpg?f=xl",
@@ -14,13 +15,30 @@ const Home:React.FC =()=> {
   const [images, setImages] = useState(imagesArr);
 
   return (
-      <MainImageContext.Provider value={{ images, setImages }}>
-        <View>
+    <MainImageContext.Provider value={{ images, setImages }}>
+      <ScrollView
+        contentContainerStyle={styles.scrollContainer}
+        showsVerticalScrollIndicator={false} // Hides the vertical scroll indicator
+        bounces={true} // Enables a smooth bounce effect
+        overScrollMode="always"
+      >
+        <View style={styles.container}>
           <ProductImages />
           <ColorSelect />
+          <ProductDescription />
         </View>
-      </MainImageContext.Provider>
-
+      </ScrollView>
+    </MainImageContext.Provider>
   );
 }
 export default Home;
+
+const styles = StyleSheet.create({
+  scrollContainer: {
+    flexGrow: 1,
+  },
+  container: {
+    flexDirection: "column",
+    justifyContent: "center",
+  },
+});
---
+++ components/screens/ProductDescription.tsx
@@ -0,0 +1,67 @@
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
+import AntDesign from "@expo/vector-icons/AntDesign";
+import { COLORS } from "@/constants/Colors";
+import SPACING from "@/styles/spacing";
+import TYPOGRAPHY from "@/styles/fontSizes";
+
+
+const ProductDescription:React.FC = () => {
+     const [isToggleDescription, setIsToggleDescription] = useState<boolean>(false);
+
+     const toggleDescription = () => {
+       setIsToggleDescription(!isToggleDescription);
+     };
+    return (
+      <>
+        <View style={styles.container}>
+          <View style={styles.headingContainer}>
+            <Text style={styles.heading}>Product Description</Text>
+            <TouchableOpacity onPress={toggleDescription}>
+              <AntDesign name={!isToggleDescription ? "down" : "up"} size={24} color={COLORS.black.primary} />
+            </TouchableOpacity>
+          </View>
+          {isToggleDescription && (
+            <Text style={styles.description}>
+              Choose a stylish dark color or brighten up your home with colorful
+              sarongs. The EKERÖ armchair has a sleek and modern look with two
+              sides that meet at the back – and a waist support for added
+              comfort!
+            </Text>
+          )}
+        </View>
+        <View style={styles.borderBottom}></View>
+      </>
+    );
+}
+
+export default ProductDescription
+
+const styles = StyleSheet.create({
+  container: {
+    padding: SPACING.SCALE_16,
+    gap: SPACING.SCALE_10,
+    backgroundColor: COLORS.white.secondary,
+  },
+  headingContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  heading: {
+    fontFamily: "manrope-regular",
+    fontWeight: "700",
+    fontSize: TYPOGRAPHY.FONT_20,
+    color:COLORS.black.primary
+  },
+  description: {
+    fontFamily: "manrope-regular",
+    fontWeight: "400",
+    fontSize: TYPOGRAPHY.FONT_16,
+    color:COLORS.black.primary
+  },
+  borderBottom: {
+    height: 1,
+    backgroundColor: "#E0E0E0",
+  },
+});
